5c031b453f87986ad54e5382dd1869c053ebf351
AlessioBarsi <160592653+AlessioBarsi@users.noreply.github.com>
2025-08-25 18:21:38 +0200

    chore: updated prisma files

--- src/app/generated/prisma/edge.js
+++ src/app/generated/prisma/edge.js
@@ -206,11 +206,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:123@localhost:5432/mydb?schema=public"
+        "value": null
       }
     }
   },
